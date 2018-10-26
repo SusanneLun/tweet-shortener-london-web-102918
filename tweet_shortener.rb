7afe481516329cce60654137d3985b22a42f579f
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 16:50:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ def word_substituter(tweets)
   tweets = []
   subtweets = []
   tweets.each do |key, value| 
-    if key == dictionary[key] do subtweets << value
+    if key == dictionary[key] subtweets << value
    end
   end 
   return subtweets.to_s 
