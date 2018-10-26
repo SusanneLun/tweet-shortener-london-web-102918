ee62c1044798c997910b73f78ee1a74a924a7102
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 17:00:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,26 +15,30 @@ end
 
 
 def word_substituter(tweets)
-  tweets = []
-  subtweets = []
-  tweets.each do |key, value| 
-    if key == dictionary[key] subtweets << dictionary[key][value]
-   end
-  end 
-  return subtweets.to_s 
+  words = tweets.split(" ")
+  short_tweet = []
+   words.each do |word|
+    case word.downcase
+    when "hello"
+      short_tweet << "hi"
+    when "to", "two", "too"
+      short_tweet << "2"
+    when "for", "four" 
+      short_tweet << "4"
+    when "be" 
+      short_tweet << "b"
+    when "you" 
+      short_tweet << "u"
+    when "at" 
+      short_tweet << "@" 
+    when "and" 
+      short_tweet << "&"
+    else
+      short_tweet << word
+    end
+  end
+  short_tweet.join(" ")
 end
 
-def word_substituter(tweet)
-  output_array = [] # creating an empty array in which to dumb the output	  output_array = [] # creating an empty array in which to dumb the output
-  # assume tweet is a regular sentence	  # assumes tweet is a regular sentence
-  output_tweet = tweet.split(" ") # returns an array	  # split returns an array, then we iterate over the array
-  output_array = output_tweet.collect do |word| # iterate over the array	  # this is ideal for a ternary 
-    if substitutes.keys.include?(word) 	  output_array = (tweet.split(" ").collect {|word| substitutes.keys.include?(word) ? substitutes[word] : word }).join(" ")
-      word = substitutes[word]	
-    else	
-      word	
-    end	
-  end	
-  output_array.join(" ") # convert it back from array into string	
-end	end
+
  
